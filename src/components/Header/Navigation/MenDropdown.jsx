09e63930e43b7d7573ca0c0ef9110c0a09cf4330
antonious stewart <threepointengineering@gmail.com>
2021-02-28 23:29:00 -0500

    add dropdown menu

---
+++ src/components/Header/Navigation/MenDropdown.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const MenDropdown = () => {
+  return <div className="dropdown-menu">men</div>;
+};
---
+++ src/components/Header/Navigation/WomenDropDown.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const WomenDropDown = () => {
+  return <div className="dropdown-menu">women</div>;
+};
