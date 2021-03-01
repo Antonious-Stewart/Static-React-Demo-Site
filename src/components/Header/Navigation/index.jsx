21c7d5dc0e80ef22562d2d6777ad1ae8fdd8bce6
antonious stewart <threepointengineering@gmail.com>
2021-02-28 23:28:16 -0500

    add more links and remove bg-dark

--- src/components/Header/Navigation/index.jsx
+++ src/components/Header/Navigation/index.jsx
@@ -1,8 +1,10 @@
 import { Link } from "react-router-dom";
 import TBD from "../../../assets/images/time-icon-TBD.png";
+import { MenDropdown } from "./MenDropdown";
+import { WomenDropDown } from "./WomenDropDown";
 export const Navigation = () => {
   return (
-    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+    <nav className="navbar navbar-expand-lg navbar-light bg-transparent">
       <Link className="navbar-brand">
         <img src={TBD} alt="" className="global-header-navbar__brand" />
       </Link>
@@ -18,7 +20,7 @@ export const Navigation = () => {
         <span className="navbar-toggler-icon"></span>
       </button>
       <div
-        className=" global-header-navbar__navbar collapse navbar-collapse justify-content-end"
+        className=" global-header-navbar__navbar collapse navbar-collapse justify-content-around position-relative"
         id="navbarSupportedContent"
       >
         <ul className="navbar-nav global-header-navbar__nav">
@@ -26,13 +28,32 @@ export const Navigation = () => {
             <Link className="nav-link">Home</Link>
           </li>
           <li className="nav-item">
-            <Link className="nav-link">About</Link>
+            <Link className="nav-link">Shop</Link>
+          </li>
+          <li className="nav-item dropdown">
+            <Link className="nav-link " href="#" data-toggle="dropdown-women">
+              Women
+            </Link>
+            <WomenDropDown />
+          </li>
+          <li className="nav-item dropdown">
+            <Link className="nav-link" data-toggle="dropdown-men">
+              Men
+            </Link>
+            <MenDropdown />
+          </li>
+
+          <li className="nav-item">
+            <Link className="nav-link">Search</Link>
           </li>
           <li className="nav-item">
-            <Link className="nav-link">Blog</Link>
+            <Link className="nav-link">Cart</Link>
           </li>
           <li className="nav-item">
-            <Link className="nav-link">Contact</Link>
+            <Link className="nav-link">Account</Link>
+          </li>
+          <li className="nav-item">
+            <Link className="nav-link">Settings</Link>
           </li>
         </ul>
       </div>
